9a1bc51176c25af2999a4c4b396f5c13074eaa9a
soumia <soumiaullrich@gmail.com>
2021-06-10 20:01:16 +0200

    user repository done
    user repository done

---
+++ src/main/java/io/swagger/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package io.swagger.repository;
+
+import io.swagger.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    List<User> getById(Long value);
+
+    User getUserById(Long value);
+
+    User findByUsername(String value);
+}
