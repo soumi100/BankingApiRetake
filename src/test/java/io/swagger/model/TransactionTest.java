8b59d22d7cd84b1d29a8c1ae6cc99fb508e67a21
Muskan <42966101+Muskan610@users.noreply.github.com>
2021-07-03 23:07:16 +0200

    trying tests

--- src/test/java/io/swagger/model/TransactionTest.java
+++ src/test/java/io/swagger/model/TransactionTest.java
@@ -1,7 +1,65 @@
 package io.swagger.model;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTest {
+    Transaction transaction;
 
+    @BeforeEach
+    public void before() throws Exception {
+        this.transaction = new Transaction
+                ("NL01INHO00000000010", "NL01INHO00000000080",
+                        600d, "berlin dinner", 1L, Transaction.TransactionTypeEnum.TRANSFER);
+    }
+
+    @Test
+    public void createTransactionShouldNotBeNull() {
+        assertNotNull(transaction);
+    }
+    @Test
+    public void invalidTransactionTypeShouldThrowIllegalArgumentException(){
+        String TransactionTypeInput = "DEPOSIT";
+        boolean isValid = false;
+        for (Transaction.TransactionTypeEnum t : Transaction.TransactionTypeEnum.values()){
+            if (t.name().equals(TransactionTypeInput)) {
+                isValid = true;
+            }
+        }
+        if (!isValid) throw new IllegalArgumentException("Transaction type is not valid!");
+    }
+    @Test
+    public void transactionAmountShouldNotBeNull() {
+        transaction.setAmount(1.0);
+        assertNotNull(transaction.getAmount());
+    }
+    @Test
+    public void transactionAmountShouldBeAboveZero(){
+        transaction.setAmount(5.0);
+        Assert.assertTrue(transaction.getAmount() > 0);
+    }
+    @Test
+    public void transactionDescriptionShouldNotBeNull() {
+        transaction.setDescription("doja cat concert");
+        assertNotNull(transaction.getDescription());
+    }
+
+    @Test
+    public void testFromIBAN() throws Exception {
+        String expected = "NL01INHO00000000010";
+        String actual = transaction.getAccountFrom();
+        // Test if equal
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testToIBAN() throws Exception {
+        String expected = "NL01INHO00000000080";
+        String actual = transaction.getAccountTo();
+        // Test if equal
+        assertEquals(expected, actual);
+    }
 }
---
+++ src/test/java/io/swagger/service/TransactionServiceTest.java
@@ -0,0 +1,66 @@
+package io.swagger.service;
+
+import io.swagger.model.Account;
+import io.swagger.model.Transaction;
+import io.swagger.model.User;
+import io.swagger.repository.AccountRepository;
+import io.swagger.repository.TransactionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.threeten.bp.LocalDate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+public class TransactionServiceTest {
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    @InjectMocks
+    private TransactionService transactionService;
+
+    List<Transaction> transactionList = new ArrayList<>();
+    private Transaction testTransaction;
+
+
+    @BeforeEach
+    public void setup() {
+
+        this.testTransaction = new Transaction
+                ("NL01INHO00000000010", "NL01INHO00000000080",
+                        800d, "paris dinner", 1L, Transaction.TransactionTypeEnum.TRANSFER);
+
+        transactionList.add(testTransaction);
+    }
+
+    @Test
+    public void addTransaction(){
+        transactionRepository.save(testTransaction);
+        when(transactionRepository.findAll()).thenReturn(transactionList);
+        List<Transaction> transactionList1 = transactionService.getTransactions();
+        assertEquals(transactionList1, transactionList);
+    }
+
+    @Test
+    public void getTransaction(){
+        when(transactionRepository.findAll()).thenReturn(transactionList);
+        List<Transaction> transactionList1 = transactionService.getTransactions();
+        assertEquals(transactionList1, transactionList);
+    }
+
+    @Test
+    public void getTransactionByIban(){
+        transactionRepository.save(testTransaction);
+        when(transactionRepository.findTransactionByAccountFrom("NL01")).thenReturn((transactionList));
+        List<Transaction> transactions = transactionService.getTransactionByIban("NL01");
+        assertEquals(transactions, testTransaction);
+    }
+}
