bbfee1b93ccdcbf7dbeef3666a69e102317297c9
Muskan <42966101+Muskan610@users.noreply.github.com>
2021-07-05 00:14:31 +0200

    trying?

---
+++ src/test/java/io/swagger/api/TransactionControllerTest.java
@@ -0,0 +1,94 @@
+package io.swagger.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.model.Transaction;
+import io.swagger.model.TransactionDto;
+import io.swagger.model.User;
+import io.swagger.model.UserDTO;
+import io.swagger.repository.TransactionRepository;
+import io.swagger.service.AccountService;
+import io.swagger.service.AuthenticationService;
+import io.swagger.service.TransactionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.OffsetDateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class TransactionControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    @MockBean
+    private AccountService accountService;
+
+    @Autowired
+    @MockBean
+    private TransactionService transactionService;
+
+    @Autowired
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    @MockBean
+    private AuthenticationService authenticationService;
+
+    private Transaction transaction;
+
+    List<Transaction> transactionList = new ArrayList<>();
+    private Transaction testTransaction;
+    ObjectMapper objectMapper;
+
+
+    private User user;
+
+    @BeforeEach
+    public void setup() {
+        user = new User(2L, "prinsalvino", "test123", "prins", "alvino", "prinsalvino@gmx.com", LocalDate.of(1993, 8, 02), "Rijswijk", "1156AX", "Amsterdam", "062535199", User.TypeEnum.EMPLOYEE, true);
+
+        this.testTransaction = new Transaction
+                ("NL01INHO00000000010", "NL01INHO00000000080",
+                        800d, "paris dinner", 1L, Transaction.TransactionTypeEnum.TRANSFER);
+
+        transactionList.add(testTransaction);
+        //transactionList.forEach(transactionRepository::save);
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void createTransactionShouldReturn200() throws Exception {
+        TransactionDto transactionDto = new TransactionDto();
+        transactionDto.setId(1L);
+        transactionDto.setAccountFrom(testTransaction.getAccountFrom());
+        transactionDto.setAccountTo(testTransaction.getAccountTo());
+        transactionDto.setAmount(testTransaction.getAmount());
+        transactionDto.setDescription(testTransaction.getDescription());
+        transactionDto.setUserPerformingId(testTransaction.getUserPerformingId());
+        OffsetDateTime dtm = OffsetDateTime.now();
+        transactionDto.setTimestamp(dtm);
+        this.mockMvc.perform(post("/transactions").contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(transactionDto))).andExpect(status().isOk());
+    }
+
+    /*@Test
+    public void transactionRepositoryNotNull(){
+        assertNotNull(transactionRepository);
+    } */
+}
