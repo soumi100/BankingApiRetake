6723b355177a9a817a2f970569f0a32bd3ec5f93
soumia <soumiaullrich@gmail.com>
2021-07-05 09:51:44 +0200

    ok
    ok

--- src/test/java/io/swagger/api/AccountApiControllerTest.java
+++ src/test/java/io/swagger/api/AccountApiControllerTest.java
@@ -2,6 +2,7 @@ package io.swagger.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.model.Account;
+import io.swagger.model.User;
 import io.swagger.service.AccountService;
 import io.swagger.service.AuthenticationService;
 import io.swagger.service.UserService;
@@ -18,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.threeten.bp.LocalDate;
 
 import java.util.Arrays;
 
@@ -46,11 +48,14 @@ class AccountApiControllerTest {
     private AuthenticationService authenticationService;
 
     private Account account1;
+    private User user;
 
     @BeforeEach
     public void setUp()  {
         account1 = new Account(1l, Account.TypeEnum.CURRENT,
                 Account.CurrencyEnum.EUR, true, "NL01INHO00000000010", 9989);
+        user = new User(2L, "prinsalvino", "test123", "prins", "alvino", "prinsalvino@gmx.com", LocalDate.of(1993, 8, 02), "Rijswijk", "1156AX", "Amsterdam", "062535199", User.TypeEnum.EMPLOYEE, true);
+
 
     }
 
@@ -97,6 +102,14 @@ class AccountApiControllerTest {
                 .perform(delete("/accounts/NL01INHO00000000010")
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk());
+    }
+    @Test
+    @WithMockUser(username = "SB", password = "123", authorities = "ROLE_CUSTOMER")
+    public void callingGetAccountByIbanShouldReturn403() throws Exception {
+        Mockito.when(authenticationService.isEmployee()).thenReturn(false);
+        Mockito.when( authenticationService.getCurrentUser()).thenReturn(user);
+        Mockito.when(accountService.getAccountByIban("NL01INHO00000000010")).thenReturn(account1);
+        this.mockMvc.perform(get("/accounts/NL01INHO00000000010")).andExpect(status().isForbidden());
     }
 
 }
